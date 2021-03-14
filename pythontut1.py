ce9cabd6f4c36ea38f9b87b7d4cc9d9b1a78c8ff
Lupercio421 <daniel.llupercio@gmail.com>
2021-03-13 23:04:34 -0500

    commit this

---
+++ pythontut1.py
@@ -0,0 +1,28 @@
+
+import sys as sys
+
+my_age = 43
+my_name = "Loopy"
+
+print("Hello", my_name)
+
+# comment
+
+'''
+Multiline comment
+'''
+
+str_1 = "\"This is a quote\""
+
+print(str_1)
+
+'''
+Newline: \n
+Backslash: \\
+Singel quote: \'
+Backspace: \b
+Tab" \t
+'''
+
+
+print(sys.maxsize)
