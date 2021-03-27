dbe62aa4544bce1e1fc907d3839db68bb5406d26
Lupercio421 <daniel.llupercio@gmail.com>
2021-03-26 22:51:32 -0400

    section 5

---
+++ section_5.py
@@ -0,0 +1,67 @@
+# drink = input("Pick One (Coke or Pepsi) : ")
+
+# if drink == "Coke":
+#     print("Here is your coke")
+# elif drink == "Pepsi":
+#     print("Here is your Pepsi")
+# else: 
+#     print("Here is your Water")
+
+# Enter Calculation : 5*6
+
+# 5*6 = 30
+
+# num_1, operator, num_2 = input("Enter Calculation : ").split()
+
+# num_1 = int(num_1)
+# num_2 = int(num_2)
+
+# if operator == "+":
+#     print("{} + {} = {}".format(num_1, num_2, num_1 + num_2))
+
+# elif operator == "-":
+#     print("{} - {} = {}".format(num_1, num_2, num_1 - num_2))
+
+# elif operator == "*":
+#     print("{} * {} = {}".format(num_1, num_2, num_1 * num_2))
+
+# elif operator == "/":
+#     print("{} / {} = {}".format(num_1, num_2, num_1 / num_2))
+
+
+# else:
+#     print("Use either + - * or / next time")
+
+# age = int(input("Enter Age : "))
+
+# if (age >= 1) and (age <= 18):
+#     print("Important Birthday")
+
+# elif (age == 21) or (age == 50):
+#     print("Important Birthday")
+
+# elif not age > 65:
+#     print("Important Birthday")
+
+# else:
+#     print("Sorry, not an important Birthday")
+
+age = int(input("Enter Age: "))
+
+if (age < 5):
+    print
+
+elif (age == 5):
+    print("Go to Kindergarten")
+
+elif (age > 5) and (age <= 17):
+    grade = age - 5
+    print("Go to Grade {}".format(grade))
+
+else:
+    print("Go to College")
+
+
+
+
+
