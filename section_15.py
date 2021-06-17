c2fd2e96d30eda4541a5469594723331961bbe22
Lupercio421 <daniel.llupercio@gmail.com>
2021-06-16 21:28:18 -0400

    dictionaries

---
+++ section_15.py
@@ -0,0 +1,43 @@
+# Dan_dict = {"f_name": "Dan", "l_name": "Banas", "address": "123 Main St"}
+
+# print("My Name :", Dan_dict["f_name"])
+
+# Dan_dict["address"] = "9712 32 AVE"
+
+# Dan_dict['city'] = "East Elmhurst"
+
+# print("Is there a city :", "city" in Dan_dict)
+
+# print(Dan_dict.values())
+
+# print(Dan_dict.keys())
+
+# for k, v in Dan_dict.items():
+#     print(k,v)
+
+# print(Dan_dict.get("m_name", "Not Here"))
+
+# del Dan_dict["f_name"]
+
+# for i in Dan_dict:
+#     print(i)
+
+# Dan_dict.clear()   
+
+# employees = []
+# f_name,l_name = input("Enter Employee Name : ").split()
+# employees.append({"f_name": f_name, "l_name": l_name})
+
+# customers = []
+# while True:
+#     create_entry = input("Enter Customer (Yes/No): ")
+#     create_entry = create_entry[0].lower()
+#     if create_entry == "n":
+#         break
+#     else:
+#         f_name, l_name = input("Enter Customer name : ").split()
+#         customers.append({'f_name': f_name, 'l_name': l_name})
+
+# for cust in customers:
+#     print(cust["f_name"], cust["l_name"])
+
