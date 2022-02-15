2ce3c60100714f8309f19863c3eb2d647e55e293
Daniel L <daniel.llupercio@gmail.com>
2022-02-14 22:49:21 -0500

    created a is_str function

--- section_11.py
+++ section_11.py
@@ -12,6 +12,9 @@
 # change_name()
 # print(globalname)
 
+# The try statement catches expressions. try statements can specify except clauses
+# with suites that serve as handlers for exceptions raised during the try suite;
+
 # def is_float(str_val):
 #     try:
 #         float(str_val)
@@ -23,9 +26,25 @@
 # print("Is Pi a Float: ", is_float(pi))
 
 
-def solve_eq(equation):
-    x, add, num_1, equal, num_2 = equation.split()
-    num_1, num_2 = int(num_1), int(num_2)
-    return "x = " + str(num_2 - num_1)
+# def solve_eq(equation):
+#     x, add, num_1, equal, num_2 = equation.split() #split returns a list of the words in the string 'equation'
+    #using sep as the delimiter string
+#     num_1, num_2 = int(num_1), int(num_2)
+#     return "x = " + str(num_2 - num_1)
 
-print(solve_eq("x + 4 = 9"))
+# print(solve_eq("x + 4 = 9"))
+
+
+def is_str(str_val):
+    try:
+        str(str_val)
+        return True
+    except ValueError:
+        return False
+
+string = 'Danny'
+print("Is Danny a String ", is_str(string))
+string2 = 21
+print("Is 21 a String ", is_str(string2))
+print(str(21)) # This was going to work anyway, as str(21) will return 21 as a string
+print(list("Danny"))
